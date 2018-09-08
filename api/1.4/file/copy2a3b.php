feed677aca0ed9c3c42c0bdea5562c7aad624a4b
PvPSteven <39722169+PvPSteven@users.noreply.github.com>
2018-09-08 13:28:16 -0400

    2
    main fies

---
+++ api/1.4/file/copy2a3b.php
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<response><action>file/copy</action><message>Session Token is missing</message><error>104</error><result>Error</result><current_api_version>1.5</current_api_version></response>
---
+++ api/1.4/folder/update2a3b.php
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<response><action>folder/update</action><message>Required parameters for this request are missing</message><error>128</error><result>Error</result><current_api_version>1.5</current_api_version></response>
